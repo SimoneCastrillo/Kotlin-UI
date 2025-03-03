759696c0787941572ecc715406572b79a80885bc
EduardoPulcino <eduardo.pulcino@sptech.school>
2025-03-02 21:08:38 -0300

    Tela login

---
+++ app/src/main/java/com/example/api/MainActivity.kt
@@ -0,0 +1,240 @@
+package com.example.api
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.api.ui.theme.APITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            APITheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Login(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Login(name: String, modifier: Modifier = Modifier) {
+    var email by remember { mutableStateOf("") }
+    var senha by remember { mutableStateOf("") }
+    var isChecked by remember { mutableStateOf(false) }
+
+
+    Column {
+        BoxWithConstraints(
+            modifier = Modifier
+                .width(412.dp)
+                .height(307.dp)
+                .background(color = Color(0xFFC54477), shape = RoundedCornerShape(topStart = 0.dp, topEnd = 0.dp, bottomStart = 0.dp, bottomEnd = 0.dp))
+                .padding(start = 80.dp, top = 63.dp, end = 80.dp, bottom = 63.dp)
+        ) {
+            val boxWidth = maxWidth
+            val boxHeight = maxHeight
+
+            Image(
+                painter = painterResource(id = R.drawable.logo_branco),
+                contentDescription = "Descrição da Imagem",
+                modifier = Modifier
+                    .width(boxWidth)
+                    .height(boxHeight),
+                contentScale = ContentScale.Fit
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .width(412.dp)
+                .height(543.dp)
+                .padding(start = 64.dp, top = 64.dp, end = 64.dp, bottom = 64.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                TextField(
+                    modifier = Modifier
+                        .background(Color(0x33D9D9D9)),
+                    value = "",
+                    onValueChange = {},
+                    label = { Text("Email") }
+                )
+                Spacer(modifier = Modifier.height(28.dp))
+                TextField(
+                    modifier = Modifier
+                        .background(Color(0x33D9D9D9)),
+                    value = "",
+                    onValueChange = {},
+                    label = { Text("Senha") }
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Box(modifier = Modifier
+                    .width(305.dp)
+                    .height(13.dp)
+                    .padding(start = 5.dp, end = 5.dp)
+                ){
+                    Row(
+                        modifier = Modifier.
+                        fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Row(
+                            modifier = Modifier.
+                            fillMaxHeight()
+                        ){
+                            Checkbox(
+                                modifier = Modifier
+                                    .height(10.dp)
+                                    .width(10.dp)
+                                    .scale(0.5f),
+                                checked = isChecked,
+                                onCheckedChange = { checked ->
+                                    isChecked = checked // Atualiza o estado quando o usuário marcar/desmarcar
+                                }
+                            )
+
+                            Spacer(modifier = Modifier.width(4.dp))
+
+                            Text(
+                                text = "Salvar acesso",
+                                style = TextStyle(
+                                    fontSize = 11.sp,
+                                    fontWeight = FontWeight(400),
+                                    color = Color(0xFF1E1E1E),
+                                    textAlign = TextAlign.Left,
+                                    textDecoration = TextDecoration.Underline,
+                                )
+                            )
+                        }
+
+                        Text(
+                            text = "Esqueci minha senha",
+                            style = TextStyle(
+                                fontSize = 11.sp,
+                                fontWeight = FontWeight(400),
+                                color = Color(0xFF1E1E1E),
+
+                                textAlign = TextAlign.Right,
+                                textDecoration = TextDecoration.Underline,
+                            )
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(46.dp))
+
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(40.dp)
+                ){
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults.buttonColors(
+                            Color.Transparent,
+                        ),
+                        modifier = Modifier
+                            .width(412.dp)
+                            .height(40.dp)
+                            .background(color = Color(0xFFC54477), shape = RoundedCornerShape(size = 16.dp))
+                            .padding(start = 16.dp, top = 2.dp, end = 16.dp, bottom = 2.dp)
+                    ){
+                        Text(
+                            text = "LOGIN",
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight(800),
+                                color = Color.White,
+                                textAlign = TextAlign.Center,
+                            )
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(122.dp))
+
+                Box(
+                    modifier = Modifier
+                        .width(284.dp)
+                        .height(13.dp)
+                ){
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = "Ainda não possui uma conta? Cadastre-se",
+                        style = TextStyle(
+                            fontSize = 11.sp,
+                            fontWeight = FontWeight(400),
+                            color = Color(0xFF000000),
+                            textAlign = TextAlign.Center,
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    APITheme {
+        Login("Android")
+    }
+}
